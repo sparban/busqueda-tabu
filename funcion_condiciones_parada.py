76861666fdd3da83a56f847904645aa6f27b762a
sparban <estebanarbe55@gmail.com>
2022-10-15 15:34:32 -0500

    Agrego funcion de condicion de parada

--- funcion_condiciones_parada.py
+++ funcion_condiciones_parada.py
@@ -4,7 +4,7 @@ import random
 contEvaluacion = 0
 s = []
 # definimos el vector soluciion  inicial 
-for i in range(5):
+for i in range(10):
     s.append(round(random.uniform(-100,100),2))      
 print("Vector solucion inicial:", s)
 
@@ -13,14 +13,15 @@ print("Vector solucion inicial:", s)
 def unimodalSeparable(s, contEvaluacion):
     acum = 0
     y =0
+    contEvaluacion += 1
     for i in range(len(s)):
         acum = (s[i]+acum)
         y = acum*acum + y
-        contEvaluacion += 1
     return(y, contEvaluacion)
 
 for iteracion in range (5):
     
-    QS = unimodalSeparable(s, contEvaluacion)[1]
-    
-print("Contados condicion de evaluacion: ", QS)
+    QS_parada = unimodalSeparable(s, contEvaluacion)
+    QS = QS_parada[0]
+    contEvaluacion = QS_parada[1]
+print("Contados condicion de evaluacion: ", QS, contEvaluacion)
